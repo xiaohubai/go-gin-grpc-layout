361dfa4f97e1c9b48d09f2560137750517c36fac
xiaohubai <xiaohubai@outlook.com>
2025-03-07 21:52:11 +0800

    init

--- deploy/volumes.sh
+++ deploy/volumes.sh
@@ -2,8 +2,8 @@ if [ -d "/usr/local/volumes" ];then
 	echo "文件夹: volumes已存在,无需创建"
 else
 	cp -r volumes /usr/local
-	chmod 777 /usr/local/volumes
-	chmod 777 /usr/local/volumes/mysql
-	chmod 777 /usr/local/volumes/redis/data
+	chmod -R 777 /usr/local/volumes
+	chmod -R 777 /usr/local/volumes/mysql/data
+	chmod -R 777 /usr/local/volumes/redis/data
 	echo "success"
 fi
